ab7f79cc9605c9eee8dc240bccbdeb70356d0733
Damir Shaikhattarov <damirshaikhattarov@gmail.com>
2023-06-12 00:15:15 +0300

    странный вариант

---
+++ Mindbox_test/Mindbox_test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Test_Library;
+
+namespace Mindbox_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Figure.Circle c = new Figure.Circle(2);
+            Console.WriteLine("S = " + c.GetArea());
+            Figure.Triangle t = new Figure.Triangle(1, 1, 1);
+            Console.WriteLine("S = " + t.GetArea());
+            Console.WriteLine("Треугольник прямоугольный:" + t.Is_right());
+            Figure.Triangle rt = new Figure.Triangle(3, 4, 5);
+            Console.WriteLine("S = " + rt.GetArea());
+            Console.WriteLine("Треугольник прямоугольный:" + rt.Is_right());
+
+        }
+    }
+}
---
+++ Mindbox_test/Test_Library/Figure.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Test_Library
+{
+    public class Figure
+    {
+        double a, b, c;
+        public Figure(double a)
+        {
+            this.a = a;
+            Console.WriteLine("Ваша фигура - круг");
+        }
+        public Figure(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            Console.WriteLine("Ваша фигура - треугольник");
+        }
+        public class Circle : Figure
+        {
+            public Circle(double a) : base(a) {}
+            public double GetArea()
+            {
+                return Math.Pow(a, 2) * Math.PI;
+            }
+        }
+        public class Triangle : Figure
+        {
+
+            public Triangle(double a, double b, double c) : base(a, b, c) { }
+            public double GetArea()
+            {
+                double p = (a + b + c)/2;
+                return Math.Sqrt(p*(p-a)*(p-b)*(p-c));
+            }
+            public bool Is_right()
+            {
+                if ((Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2)) || (Math.Pow(a, 2) + Math.Pow(c, 2) == Math.Pow(b, 2)) || (Math.Pow(c, 2) + Math.Pow(b, 2) == Math.Pow(a, 2))){
+                    return true;
+                }
+
+                else return false;
+            }
+        }
+    }
+}
+

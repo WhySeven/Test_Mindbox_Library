0e9d343df9ad1a9e141b69de7a33b09600f8f774
Damir Shaikhattarov <damirshaikhattarov@gmail.com>
2023-06-12 03:30:56 +0300

    end

--- Mindbox_test/Mindbox_test/Program.cs
+++ Mindbox_test/Mindbox_test/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using Test_Library;
 
 namespace Mindbox_test
@@ -7,15 +8,25 @@ namespace Mindbox_test
     {
         static void Main(string[] args)
         {
-            Figure.Circle c = new Figure.Circle(2);
-            Console.WriteLine("S = " + c.GetArea());
-            Figure.Triangle t = new Figure.Triangle(1, 1, 1);
-            Console.WriteLine("S = " + t.GetArea());
-            Console.WriteLine("Треугольник прямоугольный:" + t.Is_right());
-            Figure.Triangle rt = new Figure.Triangle(3, 4, 5);
-            Console.WriteLine("S = " + rt.GetArea());
-            Console.WriteLine("Треугольник прямоугольный:" + rt.Is_right());
+            //Юнит-тесты
 
+            //Проверка круга
+            Figure.Circle c = new Figure.Circle(2);
+            Console.WriteLine("Площадь круга = " + c.GetArea() +"\n");
+
+            //Проверка треугольника
+            Figure.Triangle t = new Figure.Triangle(1, 1, 1);
+            Console.WriteLine("Площадь треугольника (A = 1, B = 1, C = 1) = " + t.GetArea());
+            Console.WriteLine("Треугольник прямоугольный:" + t.Is_right() + "\n");
+
+            Figure.Triangle rt = new Figure.Triangle(3, 4, 5);
+            Console.WriteLine("Площадь треугольника (A = 3, B = 4, C = 5) = " + rt.GetArea());
+            Console.WriteLine("Треугольник прямоугольный:" + rt.Is_right() + "\n");
+
+            //Вычисление площади фигуры без знания типа фигуры в compile-time
+            Figure unknown_figure = new Figure();
+            unknown_figure.GetArea();
         }
+
     }
 }
--- Mindbox_test/Test_Library/Figure.cs
+++ Mindbox_test/Test_Library/Figure.cs
@@ -4,31 +4,66 @@ namespace Test_Library
 {
     public class Figure
     {
-        double a, b, c;
-        public Figure(double a)
-        {
-            this.a = a;
-            Console.WriteLine("Ваша фигура - круг");
-        }
-        public Figure(double a, double b, double c)
-        {
-            this.a = a;
-            this.b = b;
-            this.c = c;
-            Console.WriteLine("Ваша фигура - треугольник");
-        }
-        public class Circle : Figure
-        {
-            public Circle(double a) : base(a) {}
-            public double GetArea()
+        //При создании нового типа фигур нужно добавить наименование его класса в этот массив
+        public static String[] figure_list = { "Circle", "Triangle" };
+        string figure_type;
+        public void GetArea() {
+            Console.WriteLine("Введите один из вариантов:\n");
+            for (int i = 0; i < Figure.figure_list.Length; i++)
             {
-                return Math.Pow(a, 2) * Math.PI;
+                Console.WriteLine(Figure.figure_list[i]+"\n");
+            }
+            figure_type = FigureChoose();
+            //Также требуется кейс новой фигуры сюда для вычисления площади фигуры без знания типа фигуры в compile-time 
+            switch (figure_type)
+            {
+                case "Circle":
+                    Console.WriteLine("Введите радиус: ");
+                    Circle c = new Circle();
+                    Console.WriteLine("Площадь круга = " + c.GetArea());
+                    break;
+                case "Triangle":
+                    Console.WriteLine("Введите стороны треугольника: ");
+                    Triangle t = new Triangle();
+                    Console.WriteLine("Площадь треугольника = " + t.GetArea());
+                    break;
             }
         }
-        public class Triangle : Figure
+        //Класс кргуа
+        public class Circle
         {
-
-            public Triangle(double a, double b, double c) : base(a, b, c) { }
+            double r;
+            public Circle()
+            {
+                Console.Write("R = ");
+                r = Convert.ToDouble(Console.ReadLine());
+            }
+            public Circle(double r) {
+                this.r = r;
+            }
+            public double GetArea()
+            {
+                return Math.Pow(r, 2) * Math.PI;
+            }
+        }
+        //Класс треугольника
+        public class Triangle
+        {
+            double a, b, c;
+            public Triangle()
+            {
+                Console.Write("A = ");
+                a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("B = ");
+                b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("C = ");
+                c = Convert.ToDouble(Console.ReadLine());
+            }
+            public Triangle(double a, double b, double c) {
+                this.a = a;
+                this.b = b;
+                this.c = c;
+            }
             public double GetArea()
             {
                 double p = (a + b + c)/2;
@@ -42,6 +77,19 @@ namespace Test_Library
 
                 else return false;
             }
+        }
+
+        // Метод для выбора пользователем фигуры
+        private static string FigureChoose()
+        {
+            string figure_type = null;
+            string entered_msg = Console.ReadLine();
+            for (int i = 0; i < Figure.figure_list.Length; i++)
+            {
+                if (entered_msg == Figure.figure_list[i]) { figure_type = entered_msg; i = Figure.figure_list.Length; }
+            }
+            if (figure_type == null) { Console.WriteLine("Попробуйте ещё раз"); FigureChoose(); return null; }
+            else { Console.WriteLine("Вы выбрали:" + figure_type); return figure_type; }
         }
     }
 }

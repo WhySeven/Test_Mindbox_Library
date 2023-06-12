183833896db2a4d81d0345954356551c409fe22b
Damir Shaikhattarov <damirshaikhattarov@gmail.com>
2023-06-12 22:24:37 +0300

    sql correction

--- Test_3_SQL.sql
+++ Test_3_SQL.sql
@@ -8,8 +8,7 @@ VALUES
 	(1, 'Электроника'),
 	(2, 'Еда'),
 	(3, 'Телефоны'),
-	(4,'Мясо'),
-	(5,'Всё');
+	(4,'Мясо');
 
 CREATE TABLE Product (
 	Id INT PRIMARY KEY,
@@ -20,11 +19,12 @@ CREATE TABLE Product (
 );
 INSERT INTO Product
 VALUES
-	(1, 'Смартфон',1,3,5),
-	(2, 'Ноутбук',1,5,null),
-	(3, 'Молоко',2,5,null),
-	(4,'Колбаса',2,4,5);
-Select Product.Name as Наименование, Category.Name as Категория from Category right join Product on Product.Category1_id = Category.Id or Product.Category2_id = Category.Id or Product.Category3_id = Category.Id Where Product.Category1_id is not null ;
+	(1, 'Смартфон',1,3,null),
+	(2, 'Ноутбук',1,null,null),
+	(3, 'Молоко',2,null,null),
+	(4,'Колбаса',2,4,null),
+	(5, 'Книга',null,null,null);
+Select Product.Name as Наименование, Category.Name as Категория from Category right join Product on Product.Category1_id = Category.Id or Product.Category2_id = Category.Id or Product.Category3_id = Category.Id;
 
 Drop table product;
 Drop table category;
